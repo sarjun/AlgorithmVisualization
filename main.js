b59eceb5a95b050e879441dfe8ae905e03a66421
Arjun Shankar <sarjun93@gmail.com>
2014-07-04 16:51:08 -0400

    Implemented code that can trace the execution of mergesort (and any other divide and conquer algorithm that only manipulates the input list)

---
+++ main.js
@@ -0,0 +1,100 @@
+var util = require('util'); //required to print to console
+
+function print(msg) {
+    util.print(msg);
+}
+
+// Begin real code
+
+function getEmptyExecutionFrame() {
+	return {
+		parentFrame:null,
+		start:null,
+		divisions:[],
+		result:null
+	};
+}
+
+function printFrame(frame, indents) {
+	printTabs(indents);
+	print("{\n");
+	printTabs(indents);
+	print(frame["start"]);
+	print("\n");
+	for(var i = 0; i<frame["divisions"].length; i++) {
+		printFrame(frame["divisions"][i], indents+1);
+	}
+	printTabs(indents);
+	print(frame["result"]);
+	print("\n");
+	printTabs(indents);
+	print("}\n");
+}
+
+function printTabs(count) {
+	for(var i = 0; i < count; i++) print("\t");
+}
+
+
+function Tracker() {
+	this.execution = getEmptyExecutionFrame();
+	this.currentFrame = this.execution;
+}
+
+Tracker.prototype.traceExecution = function() {
+	printFrame(this.execution, 0);
+}
+
+Tracker.prototype.logEntry = function(list) {
+	var newFrame = getEmptyExecutionFrame();
+	newFrame["parentFrame"] = this.currentFrame;
+	this.currentFrame["divisions"].push(newFrame);
+	this.currentFrame = newFrame;
+	this.currentFrame["start"] = list;
+}
+
+Tracker.prototype.logExit = function(list) {
+	this.currentFrame["result"] = list;
+	this.currentFrame = this.currentFrame["parentFrame"];
+}
+
+function Container() {
+	this.tracker = new Tracker();
+}
+
+Container.prototype.runDivideAndConquer = function() {
+	print("currentList: " + arguments[0] + "\n");
+	this.tracker.traceExecution();
+	print("\n");
+	this.tracker.logEntry(arguments[0]);
+	var result = this["dAndC"].apply(this, arguments);
+	this.tracker.logExit(result);
+
+	return result;
+}
+
+Container.prototype.dAndC = function(list) {
+	// Divide and conquer algorithm
+	// eventually this would come from the web page
+	// for now, we will use mergesort
+	if(list.length == 1) return list;
+	var firstHalf = list.slice(0, list.length/2);
+	var secondHalf = list.slice(list.length/2);
+	firstHalf = this.runDivideAndConquer(firstHalf);
+	secondHalf = this.runDivideAndConquer(secondHalf);
+
+	var sorted = [];
+	var first = 0, second = 0;
+	for(var i=0; i<list.length; i++) {
+		if(first > firstHalf.length) sorted.push(secondHalf[second++]);
+		if(second > secondHalf.length) sorted.push(firstHalf[first++]);
+		if(firstHalf[first] > secondHalf[second]) sorted.push(secondHalf[second++]);
+		else sorted.push(firstHalf[first++]);
+	}
+
+	return sorted;
+}
+
+var test = new Container();
+test.runDivideAndConquer([8, 7, 6, 5, 4, 3, 2, 1]);
+test.tracker.traceExecution();
