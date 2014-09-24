5b5384f36eefe18bbe68c3087a30ee84d0a20eb0
Arjun Shankar <sarjun93@gmail.com>
2014-09-24 01:43:37 -0400

    Refactored out the usage of a json file for data for visualization

--- scripts/zoomable_pack.js
+++ scripts/zoomable_pack.js
@@ -16,42 +16,56 @@ var vis = d3.select("body").insert("svg:svg", "h2")
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("data.json", function(data) {
-  node = root = data;
+data = {
+	"name": "root",
+	"name2": "root2",
+	"children": [
+		{
+			"name": "level1_1",
+			"name2": "level1_1a",
+			"size": 10
+		},
+		{
+			"name": "level1_2",
+			"name2": "level1_2a",
+			"size": 10
+		}
+	]
+};
 
-  var nodes = pack.nodes(root);
+node = root = data;
+var nodes = pack.nodes(root);
 
-  vis.selectAll("circle")
-      .data(nodes)
-    .enter().append("svg:circle")
-      .attr("class", function(d) { return d.children ? "parent" : "child"; })
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d) { return d.r; })
-      .on("click", function(d) { return zoom(node == d ? root : d); });
+vis.selectAll("circle")
+  .data(nodes)
+.enter().append("svg:circle")
+  .attr("class", function(d) { return d.children ? "parent" : "child"; })
+  .attr("cx", function(d) { return d.x; })
+  .attr("cy", function(d) { return d.y; })
+  .attr("r", function(d) { return d.r; })
+  .on("click", function(d) { return zoom(node == d ? root : d); });
 
-	var append = vis.selectAll("text")
-			.data(nodes)
-			.enter();
-	append.append("svg:text")
-      .attr("class", function(d) { return d.children ? "parent" : "child"; })
-      .attr("x", function(d) { return d.x; })
-      .attr("y", function(d) { return d.y - (d.r / 2); })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle")
-      .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
-      .text(function(d) { return d.name; });
-	append.append("svg:text")
-      .attr("class", function(d) { return d.children ? "parent" : "child"; })
-      .attr("x", function(d) { return d.x; })
-      .attr("y", function(d) { return d.y + (d.r / 2); })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle")
-      .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
-      .text(function(d) { return d.name2; });
+var append = vis.selectAll("text")
+		.data(nodes)
+		.enter();
+append.append("svg:text")
+  .attr("class", function(d) { return d.children ? "parent" : "child"; })
+  .attr("x", function(d) { return d.x; })
+  .attr("y", function(d) { return d.y - (d.r / 2); })
+  .attr("dy", ".35em")
+  .attr("text-anchor", "middle")
+  .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
+  .text(function(d) { return d.name; });
+append.append("svg:text")
+  .attr("class", function(d) { return d.children ? "parent" : "child"; })
+  .attr("x", function(d) { return d.x; })
+  .attr("y", function(d) { return d.y + (d.r / 2); })
+  .attr("dy", ".35em")
+  .attr("text-anchor", "middle")
+  .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
+  .text(function(d) { return d.name2; });
 
-  d3.select(window).on("click", function() { zoom(root); });
-});
+d3.select(window).on("click", function() { zoom(root); });
 
 function zoom(d, i) {
   var k = r / d.r / 2;
