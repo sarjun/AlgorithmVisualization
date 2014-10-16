fa8deb34a67edace071ff425b3dc59f2a171f88f
Arjun Shankar <sarjun93@gmail.com>
2014-10-16 04:44:39 -0400

    Added Material Design lib code

---
+++ polymer/HTMLImports/test/js/tests.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2013 The Polymer Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+htmlSuite('HTMLImports', function() {
+  htmlTest('html/HTMLImports.html');
+  htmlTest('html/parser.html');
+  htmlTest('html/style-links.html');
+  htmlTest('html/style-paths.html');
+  htmlTest('html/load.html');
+  htmlTest('html/load-404.html');
+  htmlTest('html/currentScript.html');
+  htmlTest('html/dedupe.html');
+  htmlTest('html/dynamic.html');
+  // TODO(sjmiles): feature not implemented currently
+  //htmlTest('html/dynamic-elements.html');
+  htmlTest('html/csp.html');
+});
